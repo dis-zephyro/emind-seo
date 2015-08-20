943356aeffaff0f4f08ac4c7906eb1147e674af8
dis-zephyro <zephyro@yandex.ru>
2015-08-20 03:45:22 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        responsiveHeight: 670,
+        responsiveWidth: 1240,
+        navigation: true,
+        navigationPosition: 'right'
+    });
+});
