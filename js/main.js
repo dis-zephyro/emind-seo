acaeced5514fd47dbdebc13f22db44c956bcd1d6
dis-zephyro <zephyro@yandex.ru>
2015-08-21 05:28:45 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $('#fullpage').fullpage({
         responsiveHeight: 670,
@@ -5,4 +6,23 @@ $(document).ready(function() {
         navigation: true,
         navigationPosition: 'right'
     });
+});
+
+
+$.validate({
+    form : '.form'
+});
+
+$(document).ready(function() {
+
+    // Анимация
+    var Android = navigator.userAgent.search(/Android/i);
+    var iPhone = navigator.userAgent.search(/iPhone/i);
+    var iPad = navigator.userAgent.search(/iPad/i);
+    if(Android != -1 || iPhone != -1 || iPad != -1) {
+
+    } else {
+
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/animate.css" />'); //подключение файла animation.css если не мобильник
+    }
 });
